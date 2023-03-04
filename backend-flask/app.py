8b747f8321d63378a255b6d560421439184ef3c2
Ravalesh Bhat <ravaleshb@gmail.com>
2023-03-04 16:47:33 +0000

    Bug fix for xray instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -37,7 +37,6 @@ provider.add_span_processor(processor)
 # X-Ray ------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # Show the console log
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -53,6 +52,8 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# X-Ray ------
+XRayMiddleware(app, xray_recorder)
 
 
 frontend = os.getenv('FRONTEND_URL')
