23ebbb4d14af43757f64a88d724f813d65e2f020
Ravalesh Bhat <ravaleshb@gmail.com>
2023-03-30 21:03:59 +0000

    ddb utility script - as on 30Mar2023

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Ravalesh Bhat', 'ravaleshb@gmail.com', 'ravalesh' ,'MOCK'),
+  ('Tony Stark', 'ravalesh@hotmail.com', 'tonystark' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'ravalesh' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
